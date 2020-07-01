6dddc1e21523d23163085e946e51f611b5b9b764
Magda Florek <madzia88f@o2.pl>
2020-07-01 09:44:01 +0200

    initial commit

---
+++ rpg-creator.py
@@ -0,0 +1,12 @@
+class Character:
+    def __init__(self, strength, dexterity, durability, intellect, discernment, charisma):
+        self.strength = strength
+        self.dexterity = dexterity
+        self.durability = durability
+        self.intellect = intellect
+        self.discernment = discernment
+        self.charisma = charisma
+
+
+if __name__ == "__main__":
+    print('-----------------------------------------')
