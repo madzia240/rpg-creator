9ae3eb94a0220ac3b5be626e88e9781611df7d44
Magda Florek <madzia88f@o2.pl>
2020-07-10 15:11:21 +0200

    UI added; team fights; json with team members

--- rpg-creator.py
+++ rpg-creator.py
@@ -1,6 +1,8 @@
 import time
 import itertools
 from abc import ABC, abstractmethod
+import json
+
 
 
 class Character(ABC):
@@ -123,8 +125,80 @@ if __name__ == "__main__":
     druzynaA.memberAdd(kaczorek)
     druzynaA.memberAdd(kubus)
     druzynaA.memberAdd(rybka)
-    druzynaA.members('strength')
-    druzynaA.members('name')
-    druzynaA.membersRases()
-    druzynaB.membersRases()
+    with open('team.json', 'r') as f:
+        team = json.load(f)
+    for member in team:
+        name = (member['name']).lower()
+        if member["rase"] == "Archer":
+            name = Archer(member["name"])
+        elif member["rase"] == "Warrior":
+            name = Warrior(member["name"])
+        elif member["rase"] == "Warlock":
+            name = Warlock(member["name"])
+        elif member["rase"] == "Tank":
+            name = Tank(member["name"])
+        druzynaB.memberAdd(name)
+    # with open('team.json', 'w') as outfile:
+    #     json.dump(team, outfile)
+    print("-"*29)
+    print("|", " "*8, "Arcadia", " "*8, "|")
+    print("-"*29)
+    print("Welcome in Arcadia World!")
+    name = input('What it is your name?\n')
+    rase = int(input("What class do your prefer?\n1. Archer\n2. Warrior\n3. Warlock\n4. Tank\n"))
+    if rase == 1:
+        name = Archer(name)
+    elif rase == 2:
+        name = Warrior(name)
+    elif rase == 3:
+        name = Warlock(name)
+    elif rase == 4:
+        name = Tank(name)
+    choose = 0
+    while choose != 4:
+        choose = int(
+            input(f'{name.name}, what do you want to do?\n1.See your stats\n2.Fight!\n3.Team options\n4.Quit game\n'))
+        if choose == 1:
+            name.characterStats()
+            time.sleep(2)
+        elif choose == 2:
+            print("Choose your opponent:")
+            for i, member in enumerate(druzynaB.teamMembers):
+                print(f"{i}: {member.name}")
+            opponent = int(input())
+            print(f"Your opponent: {druzynaB.teamMembers[opponent].name}")
+            time.sleep(2)
+            print("FIGHT!")
+            time.sleep(2)
+            fight(name, druzynaB.teamMembers[opponent])
+        elif choose == 3:
+            if name not in druzynaA.teamMembers:
+                print('You do not have your team.')
+                ans = input('Do you want to join?\n[Y]es [N]o').lower()
+                if ans == "y":
+                    druzynaA.memberAdd(name)
+                    print("You joined to team!")
+            else:
+                ans = int(input('Team options:\n1. Team Members\n2. Fight!\n3. Back'))
+                if ans == 1:
+                    druzynaA.members("name")
+                    druzynaA.membersRases()
+                elif ans == 2:
+                    teamAPoints = 0
+                    teamBPoints = 0
+                    for memberA, memberB in zip(druzynaA.teamMembers, druzynaB.teamMembers):
+                        fight(memberA, memberB)
+                        if memberA.lifePoints <= 0:
+                            teamBPoints += 1
+                        elif memberB.lifePoints <= 0:
+                            teamAPoints += 1
+                    if teamAPoints > teamBPoints:
+                        print('Team A won!')
+                    elif teamAPoints < teamBPoints:
+                        print('Team B won!')
+                    elif teamAPoints == teamBPoints:
+                        print("Tie!")
+
+
+
 
